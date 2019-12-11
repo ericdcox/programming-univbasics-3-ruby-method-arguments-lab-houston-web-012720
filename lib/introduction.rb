0ff4ccf5265633619bc33698be260e5bc9b18c74
Eric Cox <ericdcox@icloud.com>
2019-12-11 18:23:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,30 +3,19 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+def introduction(name)
+  puts "Hi, my name is #{name}"
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
 
 
-## Define a Method That Takes in and Uses an Argument
-
-#Define a method, `#introduction` that takes in the argument `name` and outputs
-#the phrase: `"Hi, my name is #{name}."`
-
-## Define a Method That Takes in and Uses Two Arguments
-
-#Define a method, `#introduction_with_language` that takes in two arguments,
-#`name` and `language` and outputs the phrase: "Hi, my name is #{name} and I am
-#learning to program in #{language}."
-
-## Define a Method With an Optional Argument
-
-#Copy the previous method and name it `#introduction_with_language_optional`. It
-that takes in two arguments,`name` and `language`, but language is optional. The
-default value for `language` will be "Ruby".
-
-## Conclusion
-
-We've talked about defining methods and passing in both required and optional
-arguments. This will be the foundation of creating a lot of functionality in
-Ruby.
 
 
 
